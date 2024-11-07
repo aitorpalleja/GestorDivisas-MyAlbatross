1caffa40f8d1eb8a32a054b93556acb3bdd53f2b
Aitor Palleja <aitor.palleja@gmail.com>
2024-11-07 20:43:25 +0100

    📝 (CustomHeader.tsx): improve CustomHeader component by displaying user's initials instead of single letter   ♻️ (CustomHeader.tsx): refactor CustomHeader to use firstName variable for better readability

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,27 +1,30 @@
-// src/components/CustomHeader.tsx
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { useThemeStore } from '../stores/themeStore';
-import { useUserData } from '../services/apiClient';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {useThemeStore} from '../stores/themeStore';
+import {useUserData} from '../services/apiClient';
 
 const CustomHeader = () => {
-  const { theme } = useThemeStore();
-  const { userData } = useUserData();
+  const {theme} = useThemeStore();
+  const {userData} = useUserData();
 
   if (!userData) return null;
 
-  // Obtener iniciales del usuario
-  const initials = `${userData.name.charAt(0)}${userData.username.charAt(0)}`;
+  const nameParts = userData.name.split(' ');
+  const firstName = nameParts[0];
+  const initials =
+    nameParts[0].charAt(0) + (nameParts[1] ? nameParts[1].charAt(0) : '');
 
   return (
-    <View style={[styles.container, { backgroundColor: theme.background }]}>
-      <Image source={require('../assets/logo.png')} style={styles.logo} /> {/* Cambiado a PNG */}
+    <View style={[styles.container, {backgroundColor: theme.backgroundHeader}]}>
+      <Image source={require('../assets/logo.png')} style={styles.logo} />
       <View style={styles.userInfo}>
-        <Text style={[styles.greeting, { color: theme.text }]}>Hi,</Text>
-        <Text style={[styles.name, { color: theme.text }]}>{userData.name}</Text>
+        <Text style={[styles.greeting, {color: theme.text}]}>Hi,</Text>
+        <Text style={[styles.name, {color: theme.text}]}>{firstName}</Text>
       </View>
-      <View style={[styles.initialsBox, { backgroundColor: theme.primary }]}>
-        <Text style={[styles.initialsText, { color: theme.background }]}>{initials}</Text>
+      <View style={[styles.initialsBox, {backgroundColor: theme.primary}]}>
+        <Text style={[styles.initialsText, {color: theme.background}]}>
+          {initials.toUpperCase()}
+        </Text>
       </View>
     </View>
   );
@@ -36,21 +39,19 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
   logo: {
-    width: 100,
-    height: 30,
+    width: 130,
+    height: 40,
     resizeMode: 'contain',
   },
   userInfo: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     marginRight: 10,
-    justifyContent: 'flex-end', 
   },
   greeting: {
-    fontSize: 14, 
+    fontSize: 14,
     fontWeight: '400',
-    marginRight: 4,
+    marginBottom: 2,
   },
   name: {
     fontSize: 14,
