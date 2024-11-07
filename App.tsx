550cca28159fe530300e0a537ac134a1383887ad
Aitor Palleja <aitor.palleja@gmail.com>
2024-11-07 19:37:42 +0100

    📝 (theme.ts): Add light and dark theme color variables and configurations for theming the application

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
+import { useThemeStore } from './src/stores/themeStore';
 import './src/i18n';
 
-
 const App = () => {
-  return <AppNavigator />;
+  const { theme } = useThemeStore();
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <AppNavigator />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    primary: '#58d3a4',
+    backgroundDark: '#202327',
+    backgroundLight: '#ffffff',
+    textDark: '#ffffff',
+    textLight: '#202327',
+  };
+  
+  export const darkTheme = {
+    background: colors.backgroundDark,
+    text: colors.textDark,
+    primary: colors.primary,
+  };
+  
+  export const lightTheme = {
+    background: colors.backgroundLight,
+    text: colors.textLight,
+    primary: colors.primary,
+  };
+  
