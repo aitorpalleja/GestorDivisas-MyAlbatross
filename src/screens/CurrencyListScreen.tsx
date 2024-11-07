66367ffaef22fe5d0f75b1b56aa58d85bfb7c765
Aitor Palleja <aitor.palleja@gmail.com>
2024-11-07 18:36:52 +0100

    ✨ (CurrencyListScreen.tsx): introduce a new CurrencyListScreen component in React Native for displaying a list of currencies.

---
+++ src/screens/CurrencyListScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CurrencyListScreen = () => {
+  return (
+    <View>
+      <Text>Currency List Screen</Text>
+    </View>
+  );
+};
+
+export default CurrencyListScreen;
