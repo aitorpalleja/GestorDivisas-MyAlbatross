3d24b38f2221dec8c624bfaf6a9a3e098a48e18f
Aitor Palleja <aitor.palleja@gmail.com>
2024-11-07 20:04:51 +0100

    📝 (theme.ts): extend theme colors by adding an additional shade of background color to improve design flexibility and enhance the theme customization.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,20 +1,21 @@
 export const colors = {
-    primary: '#58d3a4',
-    backgroundDark: '#202327',
-    backgroundLight: '#ffffff',
-    textDark: '#ffffff',
-    textLight: '#202327',
-  };
-  
-  export const darkTheme = {
-    background: colors.backgroundDark,
-    text: colors.textDark,
-    primary: colors.primary,
-  };
-  
-  export const lightTheme = {
-    background: colors.backgroundLight,
-    text: colors.textLight,
-    primary: colors.primary,
-  };
-  
+  primary: '#58d3a4',
+  backgroundDark: '#202327',
+  backgroundDarker: '#181A1B',
+  backgroundLight: '#ffffff',
+  textDark: '#ffffff',
+  textLight: '#202327',
+};
+
+export const darkTheme = {
+  background: colors.backgroundDark,
+  backgroundHeader: colors.backgroundDarker,
+  text: colors.textDark,
+  primary: colors.primary,
+};
+
+export const lightTheme = {
+  background: colors.backgroundLight,
+  text: colors.textLight,
+  primary: colors.primary,
+};
