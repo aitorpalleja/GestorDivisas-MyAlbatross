f7ecc30f087764446641aad2b3cbb622e3cdd7db
Aitor Palleja <aitor.palleja@gmail.com>
2024-11-07 19:59:28 +0100

    ✨ (types.ts): introduce new types for navigation screens to handle currency-related navigation and user navigation in the app.

---
+++ src/navigation/types.ts
@@ -0,0 +1,11 @@
+import { NavigatorScreenParams } from '@react-navigation/native';
+
+export type CurrencyStackParamList = {
+  CurrencyList: undefined;
+  CurrencyDetail: { currencyCode: string };
+};
+
+export type RootTabParamList = {
+  Currencies: NavigatorScreenParams<CurrencyStackParamList>;
+  User: undefined;
+};
