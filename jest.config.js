c61bb0c78a0164cc1394380aa66643e57dd343a8
Aitor Palleja <aitor.palleja@gmail.com>
2024-11-11 03:45:18 +0100

    ✅ test(jest): enhance jest configuration for react-native

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,28 @@
+// jest.config.js
+
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        babelConfig: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/android/',
+    '<rootDir>/ios/',
+  ],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/$1',
+  },
 };
